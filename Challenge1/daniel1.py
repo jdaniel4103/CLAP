63ac0c9cbd245f7a9ecac9f13f7d3222b5fded0f
john-home <jdaniel4103@gmail.com>
2020-03-19 16:43:48 -0600

    added comment header

--- Challenge1/daniel1.py
+++ Challenge1/daniel1.py
@@ -1,1 +1,5 @@
+# John Daniel
+# Challenge 1: Github Down to Business (to defeat the Huns!)
+# This script prints out a message.
+
 print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
