ee8b2c95d1cac4e1362721bfb07cabf945a8e6f5
TrstnRoj11 <tristanrojo1115@gmail.com>
2020-03-20 11:31:52 -0700

    Create rojo1.py

---
+++ Challenge1/rojo1.py
@@ -0,0 +1,6 @@
+
+# Tristan Rojo
+# Challenge 1: Github Down to Business (to defeat the Huns!)
+# This script prints out a message.
+
+print ("Pasta fact: did you know spaghetto is the singular form of spaghetti (source: Buzzfeed)")
